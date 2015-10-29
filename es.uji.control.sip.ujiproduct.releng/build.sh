12e874e575565a644cc3b241ef80d9abbf239b28
tjulian <tjulian@uji.es>
2015-10-29 10:15:05 +0100

    Se incluye licencia UJI.

--- es.uji.control.sip.ujiproduct.releng/build.sh
+++ es.uji.control.sip.ujiproduct.releng/build.sh
@@ -1,3 +1,10 @@
+#*******************************************************************************
+# Copyright © Universitat Jaume I de Castelló 2015.
+# Aquest programari es distribueix sota les condicions de llicència EUPL 
+# o de qualsevol altra que la substituisca en el futur.
+# La llicència completa es pot descarregar de 
+# https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11
+#*******************************************************************************
 ECLIPSE_HOME=$HOME/opt/eclipse_e4
 LAUNCHER_VERSION=1.3.100.v20150511-1540
 BUILDER_VERSION=3.9.100.v20150521-1524
