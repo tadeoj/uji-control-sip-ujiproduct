f0ae3304525248c6aedd709c64f2203abc1582c5
tjulian <tjulian@uji.es>
2016-01-21 09:55:10 +0100

    v20160121-0955

--- es.uji.control.sip.ujiproduct.releng/getSIPReleng.sh
+++ es.uji.control.sip.ujiproduct.releng/getSIPReleng.sh
@@ -5,7 +5,7 @@
 # La llicència completa es pot descarregar de 
 # https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11
 #*******************************************************************************
-TAG=v20160121-0930
+TAG=v20160121-0955
 cd /$HOME/releng
 git clone git@github.com:tadeoj/uji-control-sip-ujiproduct.git
 cd uji-control-sip-ujiproduct
